5ef84e08ab7bdad6724bfddaa32670bdac9df74a
Fabio Pocci <phomea@gmail.com>
2018-11-30 16:18:36 +0100

    api wip

--- app/applications/api/ApiFrontend.php
+++ app/applications/api/ApiFrontend.php
@@ -18,7 +18,8 @@ class ApiFrontend extends FrontendApplication {
     static function declareRoutes()
     {
         return [
-            "api.wrapper"  =>  new Route("home","/api/{apiquery:(.*)}",[self::class,"apiWrapper"]),
+            "api.wrapper.get"  =>  new Route("home","/api/{apiquery:(.*)}",[self::class,"apiWrapper"]),
+            "api.wrapper.post"  =>  (new Route("home","/api/{apiquery:(.*)}",[self::class,"apiWrapper"]))->method(Route::METHOD_POST),
         ];
     }
 
@@ -28,7 +29,6 @@ class ApiFrontend extends FrontendApplication {
         if( isset(Request::$headers['Token']) ){
             $token = Request::$headers['Token'];
         }
-
 
 
         $listaapi = Api::query()->getAll();
@@ -46,21 +46,61 @@ class ApiFrontend extends FrontendApplication {
                     return static::nonAutorizzato();
                 }
             }
+
+
             $r = RouterService::parseQuery("/".$params['apiquery'],$value->metodo." /".$value->url);
+
             if( $r!== false){
-                if($value->tiporisposta == "array") {
-                    $risultati = Db::$connection->fetchAll($value->query, $r);
-                }else{
-                    $risultati = Db::$connection->fetchOne($value->query, $r);
+                if(Request::getMethod() == Route::METHOD_GET){
+                    echo self::respondGet($value,$r);
+                    exit;
                 }
-                echo json_encode($risultati);
-                exit;
+                if(Request::getMethod() == Route::METHOD_POST){
+                    echo self::respondPost($value,$r,Request::getPost());
+                    exit;
+                }
+
+                if(Request::getMethod() == Route::METHOD_PUT){
+                    echo self::respondPost($value,$r,Request::getPost());
+                    exit;
+                }
             }
 
         }
 
 
         exit;
+    }
+
+    static function respondGet($api,$get,$post =[]){
+        if($value->tiporisposta == "array") {
+            $risultati = Db::$connection->fetchAll($api->query, $get);
+        }else{
+            $risultati = Db::$connection->fetchOne($api->query, $get);
+        }
+        return json_encode($risultati);
+    }
+
+    static function respondPut($api,$get,$post =[]){
+        echo "put";
+        exit;
+    }
+    static function respondPost($api,$get,$post =[]){
+
+
+        if( $api->entity_method == "save"){
+            $dati = array_merge($get,$post);
+            $e = new $api->entity($dati);
+            $e->save();
+            echo json_encode($e);
+        }
+        exit;
+        if($value->tiporisposta == "array") {
+            $risultati = Db::$connection->fetchAll($api->query, $get);
+        }else{
+            $risultati = Db::$connection->fetchOne($api->query, $get);
+        }
+        return json_encode($risultati);
     }
 
 
--- app/applications/api/entities/Api.php
+++ app/applications/api/entities/Api.php
@@ -5,6 +5,7 @@ namespace applications\api\entities;
 use applications\banner\BannerApplication;
 use core\Model;
 use core\db\Field;
+use core\services\ApplicationsService;
 
 /**
  * Class Banner
@@ -36,6 +37,16 @@ class Api extends Model {
                 ["label"=>"DELETE","value"=>"DELETE"]
             ]),
             "url"   =>  Field::text()->editable()->setHint("La base url parte da api/. Utilizza il formato {nome} per le variabili"),
+            "tipo"  =>  Field::text()->editable()->setTemplate("select")->setTemplateVar([
+                ["label" => "Query","value"=>"query"],
+                ["label" => "Entità","value"=>"entity"]
+            ]),
+            "entity"  =>  Field::text()->editable()->setTemplate("select")->setTemplateVar([static::class,"_getEntities"])->setHint("Utilizzato solo per il tipo entità"),
+            "entity_method"  =>  Field::text()->editable()->setTemplate("select")->setTemplateVar([
+                ["label"    =>"Save","value"    =>  "save"],
+                ["label"    =>"Delete","value"  =>  "delete"],
+                ["label"   =>  "Get","value"   =>  "get"]
+            ])->setHint("Utilizzato solo per il tipo entità. Metodo da richiamare"),
             "query" =>  Field::text()->editable()->setHint("La query da chiamare. Utilizza il formato :nomevariabile per le variabili"),
             "tiporisposta"   =>  Field::varchar(128)->editable()->setTemplate("select")->setTemplateVar([
                 ["label"=>"Oggetto ( corretto per select di 1 elemento)","value"=>"object"],
@@ -48,5 +59,21 @@ class Api extends Model {
 
             ])
         ];
+    }
+
+
+    static function _getEntities(){
+        $e = [];
+        foreach (ApplicationsService::$applications as $key=>$value){
+            $e = array_merge($e,$value::install());
+        }
+        $r =[];
+        foreach ($e as $k=>$value){
+            $r[]=[
+                "label"=>$value,
+                "value"=>$value
+            ];
+        }
+        return $r;
     }
 }
